1699c5e9f0e1fe10fa354ea11c4009956f793025
othman853 <anuar.yasser@gmail.com>
2016-04-26 11:36:58 -0300

    Compiling a regex pattern using re module and python's raw string notation

--- godfile.py
+++ godfile.py
@@ -1,11 +1,18 @@
 # This is a god file, no OO, no organization, no tests, just code.
+import re as regex
+
+# Compiling a Regex using the raw string python notation (with the preceding r)
+# By now, the regex accepts anything
+reservation_pattern = regex.compile(r'.')
+
+def format_line(line_number, line):
+    return
+
 def print_file(file):
     line_number = 1
     for line in file:
         print "%d - %s" % (line_number,  line)
         line_number = line_number + 1
-
-
 
 entry_file = open('entries_sample')
 print_file(entry_file)
