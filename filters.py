7783ad2cf2797e7cb93a3d9e5f8e5f5bbf81cf1b
othman853 <anuar.yasser@gmail.com>
2016-04-28 13:02:16 -0300

    Renaming date pattern

--- filters.py
+++ filters.py
@@ -2,7 +2,7 @@ import patterns
 import re as regex
 
 customer_pattern = regex.compile(patterns.CUSTOMER_TYPE)
-date_pattern = regex.compile(patterns.DATES)
+date_pattern = regex.compile(patterns.DATE)
 
 def check_reservation_file_entry(line):
     return customer_pattern.match(line.lower()) and date_pattern.search(line)
--- patterns.py
+++ patterns.py
@@ -1,2 +1,2 @@
 CUSTOMER_TYPE = r're(gular|wards)'
-DATES = r'\d{2}\w{3}\d{4}'
+DATE = r'\d{2}\w{3}\d{4}'
